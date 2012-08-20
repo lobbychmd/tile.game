092d90ff9a565a45e9db44a3c6001f368004891b
lobbychmd <lobbychmd@gmail.com>
2012-08-20 23:16:28 +0800

    1

--- public/javascripts/tilegame.js
+++ public/javascripts/tilegame.js
@@ -1,20 +1,22 @@
-﻿$.tg = {
-    state: {},
-    setup: function (mainCanvas, size) {
-        $.tg.mainCanvas = mainCanvas;
-        $.tg.size = size;
-    },
+﻿function TileGame(mainCanvas, size){
+    this.mainCanvas = mainCanvas;
+    this.size = size;
+    this.state = {};
+}
+
+TileGame.prototype = {
+    constructor: TileGame,
     blockIndex: function (length, blockLength) {
         var r = parseInt(length / blockLength);
         return r * blockLength == length ? r - 1 : r;
     },
     canvasRange: function () {
-        var Y = $.tg.realY($.tg.size.posY);
-        var X = $.tg.size.posX;
+        var Y = this.realY(this.size.posY);
+        var X = this.size.posX;
         $('canvas.bg').removeClass('cover');
         //alert( X / canvasW);
-        for (var i = $.tg.blockIndex(X, $.tg.size.canvasW); i <= $.tg.blockIndex(X + $.tg.size.scrW, $.tg.size.canvasW); i++)
-            for (var j = $.tg.blockIndex(Y, $.tg.size.canvasH); j <= $.tg.blockIndex(Y + $.tg.size.scrH, $.tg.size.canvasH); j++) {
+        for (var i = this.blockIndex(X, this.size.canvasW); i <= this.blockIndex(X + this.size.scrW, this.size.canvasW); i++)
+            for (var j = this.blockIndex(Y, this.size.canvasH); j <= this.blockIndex(Y + this.size.scrH, this.size.canvasH); j++) {
                 $('#bg_' + i + "_" + j).addClass('cover');
             }
     },
@@ -40,7 +42,7 @@
     calcTranslate: function (canvas, x, y) {
         if (x == undefined) x = canvas.attr('id').split('_')[1];
         if (y == undefined) y = canvas.attr('id').split('_')[2];
-        var translate = 'translate(' + (x * $.tg.size.canvasW - $.tg.size.posX) + 'px, ' + (y * $.tg.size.canvasH - $.tg.realY($.tg.size.posY)) + 'px)';
+        var translate = 'translate(' + (x * this.size.canvasW - this.size.posX) + 'px, ' + (y * this.size.canvasH - this.realY(size.posY)) + 'px)';
         canvas.css('-webkit-transform', translate);
         canvas.css('-moz-transform', translate);
     },
@@ -48,9 +50,9 @@
     createBG: function () {
         //var j = 0;
         //创建所有canvas
-        for (j = 0; j <= $.tg.blockIndex($.tg.size.bgH, $.tg.size.canvasH); j++)
-            for (i = 0; i <= $.tg.blockIndex($.tg.size.bgW, $.tg.size.canvasW); i++) {
-                var canvas = $("<canvas class='bg'></canvas>").attr('width', $.tg.size.canvasW + "px").attr('height', $.tg.size.canvasH + 'px')
+        for (j = 0; j <= this.blockIndex(this.size.bgH, this.size.canvasH); j++)
+            for (i = 0; i <= this.blockIndex(this.size.bgW, this.size.canvasW); i++) {
+                var canvas = $("<canvas class='bg'></canvas>").attr('width', this.size.canvasW + "px").attr('height', this.size.canvasH + 'px')
                         .appendTo('#mainSence').attr('id', "bg_" + i + "_" + j).attr('x', i).attr('y', j);
             }
     },
@@ -61,55 +63,56 @@
             var p = pos[event.which];
             if (p) {
                 if (p[0] > 0) {  //右移
-                    $.tg.size.carX += p[0];
-                    if (($.tg.size.carX + 48) > $.tg.size.scrW) {
-                        $.tg.size.carX = $.tg.size.scrW - 48;
-                        $.tg.size.posX += p[0];
-                        if (($.tg.size.posX + $.tg.size.scrW) > $.tg.size.bgW) $.tg.size.posX = $.tg.size.bgW - $.tg.size.scrW;
+                    this.size.carX += p[0];
+                    if ((this.size.carX + 48) > this.size.scrW) {
+                        this.size.carX = this.size.scrW - 48;
+                        this.size.posX += p[0];
+                        if ((this.size.posX + this.size.scrW) > this.size.bgW) this.size.posX = this.size.bgW - this.size.scrW;
                     }
                 } else if (p[0] < 0) {  //左移
-                    $.tg.size.carX += p[0];
-                    if ($.tg.size.carX < 0) {
-                        $.tg.size.carX = 0;
-                        $.tg.size.posX += p[0];
-                        if ($.tg.size.posX < 0) $.tg.size.posX = 0;
+                    this.size.carX += p[0];
+                    if (this.size.carX < 0) {
+                        this.size.carX = 0;
+                        this.size.posX += p[0];
+                        if (this.size.posX < 0) this.size.posX = 0;
                     }
                 } else if (p[1] > 0) {  //上移 
-                    $.tg.size.carY += p[1];
-                    var Y = $.tg.realY($.tg.size.carY, $.tg.size.scrH, 48);
+                    this.size.carY += p[1];
+                    var Y = this.realY(this.size.carY, this.size.scrH, 48);
                     if (Y < 0) {
-                        $.tg.size.carY = $.tg.size.scrH - 48;
-                        $.tg.size.posY += p[1];
-                        var YY = $.tg.realY($.tg.size.posY);
-                        if (YY < 0) $.tg.size.posY = $.tg.size.bgH - $.tg.size.scrH;
+                        this.size.carY = this.size.scrH - 48;
+                        this.size.posY += p[1];
+                        var YY = this.realY(this.size.posY);
+                        if (YY < 0) this.size.posY = this.size.bgH - this.size.scrH;
                     }
                 } else if (p[1] < 0) {  //下移
-                    $.tg.size.carY += p[1];
-                    var Y = $.tg.realY($.tg.size.carY, $.tg.size.scrH, 48);
-                    if ((Y + 48) > $.tg.size.scrH) {
-                        $.tg.size.carY = 0;
-                        $.tg.size.posY += p[1];
-                        var YY = $.tg.realY($.tg.size.posY);
-                        if ((YY + $.tg.size.scrH) > $.tg.size.bgH) $.tg.size.posY = 0;
+                    this.size.carY += p[1];
+                    var Y = this.realY(this.size.carY, this.size.scrH, 48);
+                    if ((Y + 48) > this.size.scrH) {
+                        this.size.carY = 0;
+                        this.size.posY += p[1];
+                        var YY = this.realY(this.size.posY);
+                        if ((YY + this.size.scrH) > this.size.bgH) this.size.posY = 0;
                     }
                 }
-                $.tg.moveBg();
-                $.tg.updateFgState();
-                $.tg.drawFg();
+                this.moveBg();
+                this.updateFgState();
+                this.drawFg();
                 return false;
             }
 
         });
     },
     realY: function (y, bg_H, scr_H) {
-        return (bg_H ? bg_H : $.tg.size.bgH) - y - (scr_H ? scr_H : $.tg.size.scrH);
+        return (bg_H ? bg_H : this.size.bgH) - y - (scr_H ? scr_H : this.size.scrH);
     },
 
     moveBg: function () {
-        $.tg.canvasRange();
+        this.canvasRange();
+        var game = this;
         $('canvas.bg.cover').each(function () {
-            $.tg.fillBG($(this)[0]);
-            $.tg.calcTranslate($(this));
+            game.fillBG ($(this)[0]);
+            game.calcTranslate($(this));
         });
         $('canvas.bg').each(function () {
             //calcTranslate($(this));
@@ -117,49 +120,49 @@
     },
 
     updateFgState: function () {
-        var X = $.tg.size.tankX - $.tg.size.posX;
-        var Y = $.tg.realY($.tg.size.tankY - $.tg.size.posY, $.tg.size.scrH, 32);
-        if ((X > 0) && (Y > 0) && (X < $.tg.size.scrW - 32) && (Y < $.tg.size.scrH - 32)) {
-            if (!$.tg.state['tank']) {
-                $.tg.state['tank'] = { highlight: true, x: X, y: Y, timeOffset: 0, fireIdx: 1 };
-                $.tg.state['bullet1'] = { fire: true, x: X, y: Y, timeOffset: 0, angel: Math.tan(($.tg.realY($.tg.size.carY, $.tg.size.scrH, 48) - Y) / ($.tg.size.carX - X)) };
+        var X = this.size.tankX - this.size.posX;
+        var Y = this.realY(this.size.tankY - this.size.posY, this.size.scrH, 32);
+        if ((X > 0) && (Y > 0) && (X < this.size.scrW - 32) && (Y < this.size.scrH - 32)) {
+            if (!this.state['tank']) {
+                this.state['tank'] = { highlight: true, x: X, y: Y, timeOffset: 0, fireIdx: 1 };
+                this.state['bullet1'] = { fire: true, x: X, y: Y, timeOffset: 0, angel: Math.tan((this.realY(this.size.carY, this.size.scrH, 48) - Y) / (this.size.carX - X)) };
             }
             else {
-                $.tg.state['tank'].x = X;
-                $.tg.state['tank'].y = Y;
-                if ($.tg.state['tank'].timeOffset == 5) {
-                    $.tg.state['tank'].timeOffset = 0;
-                    $.tg.state['tank'].fireIdx++;
-                    $.tg.state['bullet' + $.tg.state['tank'].fireIdx] = { fire: true, x: X, y: Y, timeOffset: 0, angel: Math.tan(($.tg.realY($.tg.size.carY, $.tg.size.scrH, 48) - Y) / ($.tg.size.carX - X)) };
-                } else $.tg.state['tank'].timeOffset++;
+                this.state['tank'].x = X;
+                this.state['tank'].y = Y;
+                if (this.state['tank'].timeOffset == 5) {
+                    this.state['tank'].timeOffset = 0;
+                    this.state['tank'].fireIdx++;
+                    this.state['bullet' + this.state['tank'].fireIdx] = { fire: true, x: X, y: Y, timeOffset: 0, angel: Math.tan((this.realY(this.size.carY, this.size.scrH, 48) - Y) / (this.size.carX - X)) };
+                } else this.state['tank'].timeOffset++;
             }
 
-        } else if ($.tg.state['tank']) delete $.tg.state['tank'];
+        } else if (this.state['tank']) delete this.state['tank'];
 
-        for (var i in $.tg.state) {
-            if ($.tg.state[i].fire) {
-                $.tg.state[i].timeOffset++;
-                $.tg.state[i].x = $.tg.state[i].x + ($.tg.state[i].timeOffset * 1) * Math.cos($.tg.state[i].angel);
-                $.tg.state[i].y = $.tg.state[i].y + ($.tg.state[i].timeOffset * 1) * Math.sin($.tg.state[i].angel);
+        for (var i in this.state) {
+            if (this.state[i].fire) {
+                this.state[i].timeOffset++;
+                this.state[i].x = this.state[i].x + (this.state[i].timeOffset * 1) * Math.cos(this.state[i].angel);
+                this.state[i].y = this.state[i].y + (this.state[i].timeOffset * 1) * Math.sin(this.state[i].angel);
             }
-            if (($.tg.state[i].x > 0) && ($.tg.state[i].y > 0) && ($.tg.state[i].x < $.tg.size.scrW - 32) && ($.tg.state[i].y < $.tg.size.scrH - 32)) { }
-            else { delete $.tg.state[i]; }
+            if ((this.state[i].x > 0) && (this.state[i].y > 0) && (this.state[i].x < this.size.scrW - 32) && (this.state[i].y < this.size.scrH - 32)) { }
+            else { delete this.state[i]; }
         }
 
-        if (!$.tg.state['car1'])
-            $.tg.state['car1'] = { src: '/content/images/moon_bus.PNG', x: $.tg.size.carX, y: $.tg.realY($.tg.size.carY, $.tg.size.scrH, 48) };
+        if (!this.state['car1'])
+            this.state['car1'] = { src: '/content/images/moon_bus.PNG', x: this.size.carX, y: this.realY(this.size.carY, this.size.scrH, 48) };
         else {
-            $.tg.state['car1'].x = $.tg.size.carX; $.tg.state['car1'].y = $.tg.realY($.tg.size.carY, $.tg.size.scrH, 48);
+            this.state['car1'].x = this.size.carX; this.state['car1'].y = this.realY(this.size.carY, this.size.scrH, 48);
         }
 
-        //console.log($.tg.state);
+        //console.log(this.state);
     },
 
     drawFg: function () {
-        var ctx = $.tg.mainCanvas.getContext('2d');
-        ctx.clearRect(0, 0, $.tg.size.scrW, $.tg.size.scrH);
-        for (var s in $.tg.state) {
-            var ss = $.tg.state[s];
+        var ctx = mainCanvas.getContext('2d');
+        ctx.clearRect(0, 0, this.size.scrW, this.size.scrH);
+        for (var s in this.state) {
+            var ss = this.state[s];
             if (ss.src) {
                 var img = new Image();
                 img.src = ss.src;
@@ -169,7 +172,7 @@
             } else if (ss.highlight) {
                 ctx.lineWidth = 2;
                 ctx.strokeStyle = "blue";
-                if ((ss.x > 0) && (ss.y > 0) && (ss.x < $.tg.size.scrW - 32) && (ss.x < $.tg.size.scrH - 32))
+                if ((ss.x > 0) && (ss.y > 0) && (ss.x < this.size.scrW - 32) && (ss.x < this.size.scrH - 32))
                     ctx.strokeRect(ss.x, ss.y, 32, 32);
             } else if (ss.fire) {
                 ctx.lineWidth = 1;
@@ -185,19 +188,19 @@
     },
     run: function () {
         var main = setInterval(function () {
-            //            for (var s in $.tg.state) {
-            //                var ss = $.tg.state[s];
+            //            for (var s in this.state) {
+            //                var ss = this.state[s];
             //                if (ss.fire) {
             //                    ss.timeOffset = ss.timeOffset + 1;
             //                    //if (ss.timeOffset > 5) ss.timeOffset = 0;
             //                }
             //            }
-            $.tg.updateFgState();
-            $.tg.drawFg();
+            this.updateFgState();
+            this.drawFg();
         }, 500);
-        $.tg.createBG();
-        $.tg.moveBg();
-        $.tg.keyboardInit();
-        $.tg.updateFgState();
+        this.createBG();
+        this.moveBg();
+        this.keyboardInit();
+        this.updateFgState();
     }
 }
